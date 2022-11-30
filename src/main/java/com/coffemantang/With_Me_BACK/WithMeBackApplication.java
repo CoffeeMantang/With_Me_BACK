414774f51e5ec978ce409d6e029625218f15e19c
lv2dev <lv2dev@gmail.com>
2022-11-30 11:20:56 +0900

    first commit

---
+++ src/main/java/com/coffemantang/With_Me_BACK/WithMeBackApplication.java
@@ -0,0 +1,13 @@
+package com.coffemantang.With_Me_BACK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithMeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithMeBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coffemantang/With_Me_BACK/WithMeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffemantang.With_Me_BACK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithMeBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
